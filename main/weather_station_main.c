4db56098e5282790ded76e64da8dd7be11ab23e3
SzymonKubica <szymo.kubica@gmail.com>
2023-08-13 22:19:00 +0100

    Added the basic functionality of printing the temperature to the display.

--- main/weather_station_main.c
+++ main/weather_station_main.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #include "esp_chip_info.h"
 #include "esp_event.h"
 #include "esp_flash.h"
@@ -91,180 +90,50 @@ void led_blinker_task(void *pvParameter) {
   }
 }
 
-void lcd_test_task(void *pvParameter) {
+void temperature_check_task(void *pvParameter) {
+  ESP_LOGI(DHT_TAG, "Starting DHT Task\n\n");
+  setDHTgpio(GPIO_NUM_4);
 
-  	SSD1306_t dev;
-	int center, top, bottom;
-	char lineChar[20];
+  SSD1306_t dev;
 
-#if CONFIG_I2C_INTERFACE
-	ESP_LOGI(tag, "INTERFACE is i2c");
-	ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
-	ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
-	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-	i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_I2C_INTERFACE
+  ESP_LOGI(tag, "INTERFACE is i2c");
+  ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d", CONFIG_SDA_GPIO);
+  ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d", CONFIG_SCL_GPIO);
+  ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d", CONFIG_RESET_GPIO);
+  i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
 
-#if CONFIG_SPI_INTERFACE
-	ESP_LOGI(tag, "INTERFACE is SPI");
-	ESP_LOGI(tag, "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
-	ESP_LOGI(tag, "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
-	ESP_LOGI(tag, "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
-	ESP_LOGI(tag, "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
-	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-	spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_SPI_INTERFACE
+  ESP_LOGI(tag, "Panel is 128x64");
+  ssd1306_init(&dev, 128, 64);
+  ssd1306_clear_screen(&dev, false);
 
-#if CONFIG_FLIP
-	dev._flip = true;
-	ESP_LOGW(tag, "Flip upside down");
-#endif
+  while (true) {
+    ESP_LOGI(DHT_TAG, "=== Reading DHT ===\n");
+    int ret = readDHT();
 
-#if CONFIG_SSD1306_128x64
-	ESP_LOGI(tag, "Panel is 128x64");
-	ssd1306_init(&dev, 128, 64);
-#endif // CONFIG_SSD1306_128x64
-#if CONFIG_SSD1306_128x32
-	ESP_LOGI(tag, "Panel is 128x32");
-	ssd1306_init(&dev, 128, 32);
-#endif // CONFIG_SSD1306_128x32
+    errorHandler(ret);
 
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
+    float humidity = getHumidity();
+    float temperature = getTemperature();
 
-#if CONFIG_SSD1306_128x64
-	top = 2;
-	center = 3;
-	bottom = 8;
-	ssd1306_display_text(&dev, 0, "SSD1306 128x64", 14, false);
-	ssd1306_display_text(&dev, 1, "ABCDEFGHIJKLMNOP", 16, false);
-	ssd1306_display_text(&dev, 2, "abcdefghijklmnop",16, false);
-	ssd1306_display_text(&dev, 3, "Hello World!!", 13, false);
-	//ssd1306_clear_line(&dev, 4, true);
-	//ssd1306_clear_line(&dev, 5, true);
-	//ssd1306_clear_line(&dev, 6, true);
-	//ssd1306_clear_line(&dev, 7, true);
-	ssd1306_display_text(&dev, 4, "SSD1306 128x64", 14, true);
-	ssd1306_display_text(&dev, 5, "ABCDEFGHIJKLMNOP", 16, true);
-	ssd1306_display_text(&dev, 6, "abcdefghijklmnop",16, true);
-	ssd1306_display_text(&dev, 7, "Hello World!!", 13, true);
-#endif // CONFIG_SSD1306_128x64
+    ESP_LOGI(DHT_TAG, "Hum: %.1f Tmp: %.1f\n", humidity, temperature);
 
-#if CONFIG_SSD1306_128x32
-	top = 1;
-	center = 1;
-	bottom = 4;
-	ssd1306_display_text(&dev, 0, "SSD1306 128x32", 14, false);
-	ssd1306_display_text(&dev, 1, "Hello World!!", 13, false);
-	//ssd1306_clear_line(&dev, 2, true);
-	//ssd1306_clear_line(&dev, 3, true);
-	ssd1306_display_text(&dev, 2, "SSD1306 128x32", 14, true);
-	ssd1306_display_text(&dev, 3, "Hello World!!", 13, true);
-#endif // CONFIG_SSD1306_128x32
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
+    ssd1306_contrast(&dev, 0xff);
 
-	// Display Count Down
-	uint8_t image[24];
-	memset(image, 0, sizeof(image));
-	ssd1306_display_image(&dev, top, (6*8-1), image, sizeof(image));
-	ssd1306_display_image(&dev, top+1, (6*8-1), image, sizeof(image));
-	ssd1306_display_image(&dev, top+2, (6*8-1), image, sizeof(image));
-	for(int font=0x39;font>0x30;font--) {
-		memset(image, 0, sizeof(image));
-		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
-		memcpy(image, font8x8_basic_tr[font], 8);
-		if (dev._flip) ssd1306_flip(image, 8);
-		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
+    char temperature_buffer[22];
+    snprintf(temperature_buffer, 22, "           %.1fC", temperature);
+    char humidity_buffer[22];
+    snprintf(humidity_buffer, 22, "           %.1f%%", humidity);
 
-	// Scroll Up
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
-	//ssd1306_software_scroll(&dev, 7, 1);
-	ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
-	for (int line=0;line<bottom+10;line++) {
-		lineChar[0] = 0x01;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
+    ssd1306_display_text(&dev, 1, "Weather Station", 16, false);
+    ssd1306_display_text(&dev, 3, "Temperature:", 13, false);
+    ssd1306_display_text(&dev, 4, temperature_buffer, 22, false);
+    ssd1306_display_text(&dev, 5, "Humidity:", 9, false);
+    ssd1306_display_text(&dev, 6, humidity_buffer, 22, false);
 
-	// Scroll Down
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
-	//ssd1306_software_scroll(&dev, 1, 7);
-	ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
-	for (int line=0;line<bottom+10;line++) {
-		lineChar[0] = 0x02;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
-
-	// Page Down
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
-	ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
-	for (int line=0;line<bottom+10;line++) {
-		//if ( (line % 7) == 0) ssd1306_scroll_clear(&dev);
-		if ( (line % (dev._pages-1)) == 0) ssd1306_scroll_clear(&dev);
-		lineChar[0] = 0x02;
-		sprintf(&lineChar[1], " Line %02d", line);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-	vTaskDelay(3000 / portTICK_PERIOD_MS);
-
-	// Horizontal Scroll
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "Horizontal", 10, false);
-	ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
-
-	// Vertical Scroll
-	ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "Vertical", 8, false);
-	ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_UP);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
-
-	// Invert
-	ssd1306_clear_screen(&dev, true);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
-	vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-
-	// Fade Out
-	ssd1306_fadeout(&dev);
-
-#if 0
-	// Fade Out
-	for(int contrast=0xff;contrast>0;contrast=contrast-0x20) {
-		ssd1306_contrast(&dev, contrast);
-		vTaskDelay(40);
-	}
-#endif
-
-	// Restart module
-	esp_restart();
-
-
+    // -- wait at least 2 sec before reading again ------------
+    // The interval of whole process must be beyond 2 seconds !!
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
 }
 
 void configure_gpio_outputs() {
@@ -314,9 +183,14 @@ void app_main(void) {
 
   printf("Welcome to the weather station!\n");
 
-  xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 5, NULL);
-  xTaskCreate(&led_blinker_task, "led_blinker_task", 2048, NULL, 5, NULL);
-  xTaskCreate(&lcd_test_task, "lcd_test_task", 4096, NULL, 5, NULL);
+
+  // Make sure the LED is off.
+  gpio_set_level(GPIO_OUTPUT_IO_0, 1);
+  // Disabled this task because it is done inside of the temperature_check_task
+  // xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 5, NULL);
+  // xTaskCreate(&led_blinker_task, "led_blinker_task", 2048, NULL, 5, NULL);
+  xTaskCreate(&temperature_check_task, "temperature_check_task", 4096, NULL, 5,
+              NULL);
 
   fflush(stdout);
 }
