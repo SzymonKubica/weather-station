d420d2f3b4470eb4cbbe37652c0e9a92bfee7a3a
SzymonKubica <szymo.kubica@gmail.com>
2023-08-20 13:27:33 +0100

    Added an internal logging module.

--- main/util/logging.c
+++ main/util/logging.c
@@ -1,10 +1,11 @@
 #include "logging.h"
 
-unsigned char log_run_level = LOG_LVL_DEBUG;
+unsigned char log_run_level = LOG_LVL_INFO;
 
 const char * log_level_strings [] = {
   "NONE",  // 0
   "ERROR", // 1
   "INFO",  // 2
   "DEBUG", // 3
+  "VERBOSE", // 4
 };
--- main/util/logging.h
+++ main/util/logging.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 
 enum {
-  LOG_LVL_NONE,  // 0
-  LOG_LVL_ERROR, // 1
-  LOG_LVL_INFO,  // 2
-  LOG_LVL_DEBUG, // 3
+  LOG_LVL_NONE,    // 0
+  LOG_LVL_ERROR,   // 1
+  LOG_LVL_INFO,    // 2
+  LOG_LVL_DEBUG,   // 3
+  LOG_LVL_VERBOSE, // 4
 };
 
 #ifndef LOG_BUILD_LEVEL
@@ -26,16 +27,20 @@ extern const char *log_level_strings[];
 #define LEVEL_ENABLED(level)                                                   \
   (level <= LOG_BUILD_LEVEL && level <= log_run_level)
 
-#define LOG(level, fmt, ...)                                                   \
+#define LOG(tag, level, format, ...)                                           \
   do {                                                                         \
     if (LEVEL_ENABLED(level)) {                                                \
-      printf("[%s] %s:%d: " fmt "\n", log_level_strings[level], __FUNCTION__,  \
-             __LINE__, ##__VA_ARGS__);                                          \
+      printf("%s: [%s] %s:%d: " format "\n", tag, log_level_strings[level],    \
+             __FUNCTION__, __LINE__, ##__VA_ARGS__);                           \
     }                                                                          \
   } while (0)
 
-#define LOG_INFO(fmt, ...) LOG(LOG_LVL_INFO, fmt, ##__VA_ARGS__)
-#define LOG_ERROR(fmt, ...) LOG(LOG_LVL_ERROR, fmt, ##__VA_ARGS__)
-#define LOG_DEBUG(fmt, ...) LOG(LOG_LVL_DEBUG, fmt, ##__VA_ARGS__)
+#define LOG_INFO(tag, format, ...) LOG(tag, LOG_LVL_INFO, format, ##__VA_ARGS__)
+#define LOG_ERROR(tag, format, ...)                                            \
+  LOG(tag, LOG_LVL_ERROR, format, ##__VA_ARGS__)
+#define LOG_DEBUG(tag, format, ...)                                            \
+  LOG(tag, LOG_LVL_DEBUG, format, ##__VA_ARGS__)
+#define LOG_VERBOSE(tag, format, ...)                                          \
+  LOG(tag, LOG_LVL_VERBOSE, format, ##__VA_ARGS__)
 
 #endif
