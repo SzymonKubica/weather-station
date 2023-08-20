cd0f369cfff2f687a16e43ac9b41ca6098c9a09c
SzymonKubica <szymo.kubica@gmail.com>
2023-08-20 21:58:24 +0100

    Reformatted the logging file and changed the default logging level to DEBUG

--- main/util/logging.c
+++ main/util/logging.c
@@ -1,11 +1,11 @@
 #include "logging.h"
 
-unsigned char log_run_level = LOG_LVL_INFO;
+unsigned char log_run_level = LOG_LVL_DEBUG;
 
-const char * log_level_strings [] = {
-  "NONE",  // 0
-  "ERROR", // 1
-  "INFO",  // 2
-  "DEBUG", // 3
-  "VERBOSE", // 4
+const char *log_level_strings[] = {
+    "NONE",    // 0
+    "ERROR",   // 1
+    "INFO",    // 2
+    "DEBUG",   // 3
+    "VERBOSE", // 4
 };
