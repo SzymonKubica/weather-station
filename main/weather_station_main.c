2ce6912b065001c2f14adc488bc589a319457990
SzymonKubica <szymo.kubica@gmail.com>
2023-08-22 22:47:16 +0100

    Refactored the code and fixed the xqueue communication.

--- main/weather_station_main.c
+++ main/weather_station_main.c
@@ -30,36 +30,59 @@
 #define BLINKER_TAG "LED_BLINKER"
 #define NEC_TAG "NEC"
 
+TaskHandle_t task_0_handle = NULL;
 TaskHandle_t task_1_handle = NULL;
 TaskHandle_t task_2_handle = NULL;
+
 static QueueHandle_t ir_remote_input_queue = NULL;
 
-struct IRRemotePressRegisteredMessage {
-  enum RemoteButton registeredButton;
-} remoteMessage;
+static void led_blinker_task(void *pvParameter);
+static void temperature_monitor_task(void *pvParameter);
+static void rmt_nex_rx_continuous_task();
+static void disable_led_by_default();
 
-void led_blinker_task(void *pvParameter) {
+struct QueueMessage {
+  enum RemoteButton pressed_button;
+} queueMessage;
+
+void app_main(void) {
+  configure_gpio_outputs();
+  initialize_non_volatile_flash();
+  esp_log_level_set("*", ESP_LOG_INFO);
+
+  disable_led_by_default();
+
+  ir_remote_input_queue = xQueueCreate(10, sizeof(struct QueueMessage *));
+
+  xTaskCreate(&led_blinker_task, "led", 2048, NULL, 5, &task_1_handle);
+  xTaskCreate(&temperature_monitor_task, "dht-22", 4096, NULL, 5,
+              &task_0_handle);
+  xTaskCreatePinnedToCore(&rmt_nex_rx_continuous_task, "rmt_nec_rx_task", 4096,
+                          NULL, 10, &task_2_handle, 0);
+
+  fflush(stdout);
+}
+
+static void disable_led_by_default() { gpio_set_level(GPIO_OUTPUT_IO_0, 1); }
+
+static void led_blinker_task(void *pvParameter) {
   ESP_LOGI(BLINKER_TAG, "Starting Led Blinker Task\n\n");
-  int i = 0;
-  struct IRRemotePressRegisteredMessage *rxMessage;
+  int i = 1; // Led is off by default
+  struct QueueMessage *received_message;
   while (true) {
-    if (xQueueReceive(ir_remote_input_queue, &(rxMessage), (TickType_t)2)) {
-      if (rxMessage->registeredButton == BUTTON_EQ) {
-        i = (i + 1) % 2;
-        gpio_set_level(GPIO_OUTPUT_IO_0, i % 2);
-        char *level;
-        level = (i % 2 == 1) ? "HIGH" : "LOW";
+    if (xQueueReceive(ir_remote_input_queue, &(received_message), (TickType_t)5) &&
+        received_message->pressed_button == BUTTON_EQ) {
+      i = (i + 1) % 2;
+      gpio_set_level(GPIO_OUTPUT_IO_0, i % 2);
+      char *level = (i == 0) ? "HIGH" : "LOW";
 
-        ESP_LOGI(BLINKER_TAG, "Setting the led pin %d %s\n", GPIO_OUTPUT_IO_0,
-                 level);
-      }
-
-      wait_seconds(1);
+      ESP_LOGI(BLINKER_TAG, "Setting the led pin %d %s\n", GPIO_OUTPUT_IO_0,
+               level);
     }
   }
 }
 
-void temperature_monitor_task(void *pvParameter) {
+static void temperature_monitor_task(void *pvParameter) {
   ESP_LOGI(DISPLAY_TAG, "Initialising the OLED display...\n\n");
   SSD1306_t dev;
   initialise_display(&dev);
@@ -70,7 +93,6 @@ void temperature_monitor_task(void *pvParameter) {
   while (true) {
     ESP_LOGI(DHT_TAG, "=== Reading DHT ===");
     int ret = read_dht();
-
     handle_errors(ret);
 
     float humidity = get_humidity();
@@ -79,28 +101,21 @@ void temperature_monitor_task(void *pvParameter) {
     ESP_LOGI(DHT_TAG, "Hum: %.1f Tmp: %.1f\n", humidity, temperature);
     print_temperature_and_humidity(&dev, temperature, humidity);
 
-    // -- wait at least 2 sec before reading again ------------
-    // The interval of whole process must be beyond 2 seconds !!
     wait_seconds(3);
   }
 }
 
+static void get_nec_ring_buffer(RingbufHandle_t *rb);
 
-/**
- * @brief RMT receiver demo, this task will print each received NEC data.
- *
- */
 static void rmt_nex_rx_continuous_task() {
   if (!ir_remote_input_queue) {
     ESP_LOGE(NEC_TAG, "Failed to create IR Remote Input Queue");
   }
 
-  int channel = RMT_RX_CHANNEL;
-  nec_rx_init();
   RingbufHandle_t rb = NULL;
-  // get RMT RX ringbuffer
-  rmt_get_ringbuf_handle(channel, &rb);
-  rmt_rx_start(channel, 1);
+  get_nec_ring_buffer(&rb);
+
+  struct QueueMessage *message = & queueMessage;
   while (rb) {
     size_t rx_size = 0;
     // try to receive data from ringbuffer.
@@ -117,13 +132,12 @@ static void rmt_nex_rx_continuous_task() {
                                   &rmt_addr, &rmt_cmd);
         if (res > 0) {
           offset += res + 1;
-          struct IRRemotePressRegisteredMessage *message;
-          message = &remoteMessage;
-          message->registeredButton = mapFromInt(rmt_cmd);
-          xQueueSend(ir_remote_input_queue, (void *)&message, (TickType_t)0);
           ESP_LOGI(NEC_TAG, "RMT RCV --- addr: 0x%04x cmd: 0x%04x", rmt_addr,
                    rmt_cmd);
-          ESP_LOGI(NEC_TAG, "Button press registered: %s",
+
+          message->pressed_button = mapFromInt(rmt_cmd);
+          xQueueSend(ir_remote_input_queue, (void *)&message, (TickType_t)0);
+          ESP_LOGI(NEC_TAG, "Button press registered: %s\n",
                    button_names[mapFromInt(rmt_cmd)]);
 
         } else {
@@ -136,23 +150,9 @@ static void rmt_nex_rx_continuous_task() {
   }
 }
 
-void app_main(void) {
-  configure_gpio_outputs();
-  initialize_non_volatile_flash();
-  esp_log_level_set("*", ESP_LOG_INFO);
-
-  // Make sure the LED is off.
-  gpio_set_level(GPIO_OUTPUT_IO_0, 1);
-
-  ir_remote_input_queue =
-      xQueueCreate(10, sizeof(struct IRRemotePressRegistereMessage *));
-
-  xTaskCreate(&led_blinker_task, "led_blinker", 2048, NULL, 5, &task_1_handle);
-  xTaskCreate(&temperature_monitor_task, "temperature_monitor", 4096, NULL, 5,
-              NULL);
-
-  xTaskCreatePinnedToCore(&rmt_nex_rx_continuous_task, "rmt_nec_rx_task", 4096, NULL, 10,
-              &task_2_handle, 0);
-
-  fflush(stdout);
+static void get_nec_ring_buffer(RingbufHandle_t *rb) {
+  int channel = RMT_RX_CHANNEL;
+  nec_rx_init();
+  rmt_get_ringbuf_handle(channel, rb);
+  rmt_rx_start(channel, 1);
 }
