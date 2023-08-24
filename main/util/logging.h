d16f60f432e07c9f6b13c6889e662976800ed7fe
SzymonKubica <szymo.kubica@gmail.com>
2023-08-24 22:42:33 +0100

    Introduced the 4 chars indentation in the util module.

--- main/util/logging.h
+++ main/util/logging.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 
 enum {
-  LOG_LVL_NONE,    // 0
-  LOG_LVL_ERROR,   // 1
-  LOG_LVL_INFO,    // 2
-  LOG_LVL_DEBUG,   // 3
-  LOG_LVL_VERBOSE, // 4
+    LOG_LVL_NONE,    // 0
+    LOG_LVL_ERROR,   // 1
+    LOG_LVL_INFO,    // 2
+    LOG_LVL_DEBUG,   // 3
+    LOG_LVL_VERBOSE, // 4
 };
 
 #ifndef LOG_BUILD_LEVEL
@@ -25,22 +25,23 @@ extern unsigned char log_run_level;
 extern const char *log_level_strings[];
 
 #define LEVEL_ENABLED(level)                                                   \
-  (level <= LOG_BUILD_LEVEL && level <= log_run_level)
+    (level <= LOG_BUILD_LEVEL && level <= log_run_level)
 
 #define LOG(tag, level, format, ...)                                           \
-  do {                                                                         \
-    if (LEVEL_ENABLED(level)) {                                                \
-      printf("%s: [%s] %s:%d: " format "\n", tag, log_level_strings[level],    \
-             __FUNCTION__, __LINE__, ##__VA_ARGS__);                           \
-    }                                                                          \
-  } while (0)
+    do {                                                                       \
+        if (LEVEL_ENABLED(level)) {                                            \
+            printf("%s: [%s] %s:%d: " format "\n", tag,                        \
+                   log_level_strings[level], __FUNCTION__, __LINE__,           \
+                   ##__VA_ARGS__);                                             \
+        }                                                                      \
+    } while (0)
 
 #define LOG_INFO(tag, format, ...) LOG(tag, LOG_LVL_INFO, format, ##__VA_ARGS__)
 #define LOG_ERROR(tag, format, ...)                                            \
-  LOG(tag, LOG_LVL_ERROR, format, ##__VA_ARGS__)
+    LOG(tag, LOG_LVL_ERROR, format, ##__VA_ARGS__)
 #define LOG_DEBUG(tag, format, ...)                                            \
-  LOG(tag, LOG_LVL_DEBUG, format, ##__VA_ARGS__)
+    LOG(tag, LOG_LVL_DEBUG, format, ##__VA_ARGS__)
 #define LOG_VERBOSE(tag, format, ...)                                          \
-  LOG(tag, LOG_LVL_VERBOSE, format, ##__VA_ARGS__)
+    LOG(tag, LOG_LVL_VERBOSE, format, ##__VA_ARGS__)
 
 #endif
